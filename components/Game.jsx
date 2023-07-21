42b24778bfab2c96b96516a00ea7ef08b5719331
Syed Laeeq Ahmed <92651026+LaeeqtheDev@users.noreply.github.com>
2023-07-21 04:01:08 +0100

    Update Game.jsx

--- components/Game.jsx
+++ components/Game.jsx
@@ -22,7 +22,7 @@ const Hero = () => {
         </Helmet>
         <div className="relative flex justify-center items-center">
           {/* Apply styling to center the spline-viewer */}
-          <div className="container items-center justify-center w-full" style={{ margin: '0 auto, marginLeft: '30px', height: '550px' }}>
+          <div className="container items-center justify-center w-full" style={{ margin: '0 auto, marginLeft: '35px', height: '550px' }}>
             <spline-viewer
               className="w-full"
               style={{ height: '450px' }}
