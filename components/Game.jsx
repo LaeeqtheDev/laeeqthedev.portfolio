a80ceb6467ad3f29c9f889e22e2104c45105624c
Syed Laeeq Ahmed <92651026+LaeeqtheDev@users.noreply.github.com>
2023-07-21 04:47:27 +0100

    Update Game.jsx

--- components/Game.jsx
+++ components/Game.jsx
@@ -28,7 +28,7 @@ const Hero = () => {
               margin: '0 auto',
               marginRight: '50px',
               height: '550px',
-              width: '850px',
+              width: '1050px',
             }}
           >
             <spline-viewer
