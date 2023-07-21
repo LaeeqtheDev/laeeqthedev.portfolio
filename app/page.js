17b37714e80363b82b789f86f3d3175dd030b2c2
Syed Laeeq Ahmed <92651026+LaeeqtheDev@users.noreply.github.com>
2023-07-21 03:45:48 +0100

    Updated game.js rendering

--- app/page.js
+++ app/page.js
@@ -1,3 +1,5 @@
+"use client"
+import React, { useState, useEffect } from 'react';
 import ActiveUsers from '@/components/ActiveUsers'
 import Clients from '@/components/ClientsSlider'
 import Contactus from '@/components/ContactUs'
@@ -14,22 +16,37 @@ import Testimonal from '@/components/Testimonal'
 
 
 export default function Home() {
+  const [isLargeScreen, setIsLargeScreen] = useState(true);
+
+  useEffect(() => {
+    // Function to handle the window resize event
+    const handleResize = () => {
+      setIsLargeScreen(window.innerWidth >= 992); // 992px is the breakpoint for LG screens
+    };
+
+    // Attach event listener for window resize
+    window.addEventListener('resize', handleResize);
+
+    // Call the handleResize function on component mount to set the initial screen size
+    handleResize();
+
+    // Clean up the event listener on component unmount
+    return () => {
+      window.removeEventListener('resize', handleResize);
+    };
+  }, []);
+
   return (
-      <main>
-       
-       <Hero/>
-       <Game/>
-       <Content/>
-       <Roadmap/>
-       <ActiveUsers/>
-       <Projects/> 
-       <Clients/>
-       <Testimonal/>
-       <Contactus/>
-       
-        
-      
-      
+    <main>
+      <Hero />
+      {isLargeScreen && <Game />} {/* Render Game component only if the screen is lg */}
+      <Content />
+      <Roadmap />
+      <ActiveUsers />
+      <Projects />
+      <Clients />
+      <Testimonal />
+      <Contactus />
     </main>
   )
 }
