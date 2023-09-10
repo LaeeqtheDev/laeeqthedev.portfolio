b081a965d9e1a4d1f605ef45fbaf6e2fee4a48cc
Syed Laeeq Ahmed <92651026+LaeeqtheDev@users.noreply.github.com>
2023-09-10 19:55:43 +0100

    Update Hero.jsx

--- components/Hero.jsx
+++ components/Hero.jsx
@@ -8,8 +8,8 @@ import Image from 'next/image';
 const Hero = () => {
   const [text, count] = useTypewriter({
     words: [
-      `Hi, The Name's Syed Laeeq Ahmed`,
-      "Guy-who-loves-Anime.tsx",
+      `Hi, The Name's UNICORN PRODUCTIONs`,
+      "TEAM-who-loves-Anime.tsx",
       "<ButLovesToCodeMore/>"
     ],
     loop: true,
@@ -21,7 +21,7 @@ const Hero = () => {
       <BackgroundCircles />
       <Image className='relative rounded-full mx-auto object-cover' src='images/bighead.svg' alt='' width={500} height={500} />
       <div className='z-20'>
-        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>Software Engineer/ Designer</h2>
+        <h2 className='text-sm uppercase text-gray-500 pb-2 tracking-[15px]'>DEVELOPMENT/ DESIGN</h2>
         <h1 className='text-5xl lg:text-6xl font-semibold px-10'>
           <span className='mr-3'>{text}</span>
           <Cursor cursorColor='#F7AB0A' />
