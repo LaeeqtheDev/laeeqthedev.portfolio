76941ef30945d870bb52d363eb64ec59bfa45209
Syed Laeeq Ahmed <92651026+LaeeqtheDev@users.noreply.github.com>
2023-09-10 20:00:39 +0100

    Update Content.jsx

--- components/Content.jsx
+++ components/Content.jsx
@@ -18,13 +18,13 @@ const Content = () => {
                   <Image className="rounded-full" src="/images/bighead.svg" alt="avt" width={150} height={50} />
                 </div>
                 <div className="flex flex-col items-center text-center justify-center">
-                  <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">Syed Laeeq Ahmed</h2>
+                  <h2 className="font-medium title-font mt-4 text-gray-900 text-lg">UNICORN PRODUCTIONS</h2>
                   <div className="w-12 h-1 bg-purple-500 rounded mt-2 mb-4"></div>
-                  <p className="text-base">HTML| CSS | JAVASCRIPT | REACT.JS | NEXT.JS | TAILWIND | FIREBASE | VERCEL | SANITY </p>
+                  <p className="text-base">HTML| CSS | JAVASCRIPT | MONGODB | NODE.JS | REACT.JS | NEXT.JS | TAILWIND | FIREBASE | VERCEL | SANITY </p>
                 </div>
               </div>
               <div className="sm:w-2/3 sm:pl-8 sm:py-8 sm:border-l border-gray-200 sm:border-t-0 border-t mt-4 pt-4 sm:mt-0 text-center sm:text-left">
-                <p className="leading-relaxed text-lg mb-4">I am a highly skilled Fullstack Developer with expertise in React.js and Next.js, specializing in assisting startups with their virtual setups. My passion lies in leveraging cutting-edge technologies to solve complex problems and meet specific requirements. I have a strong foundation in web development, including HTML, CSS, and Bootstrap, and extensive knowledge of JavaScript, React JS, and jQuery. I am proficient in working with database platforms like Firebase and Sanity for efficient data management. I am well-versed in software development principles, testing, and agile methodologies. Additionally, I utilize various web development tools to streamline the development process. I thrive in high-pressure environments and excel in adaptability, problem-solving, and attention to detail. Overall, I am a dedicated professional ready to contribute my expertise and drive success in any team or project.</p>
+                <p className="leading-relaxed text-lg mb-4">We are highly skilled Fullstack Developers with expertise in React.js and Next.js, specializing in assisting startups with their virtual setups. Our passion lies in leveraging cutting-edge technologies to solve complex problems and meet specific requirements. We have a strong foundation in Web Development, including HTML, CSS, and Bootstrap, and extensive knowledge of JavaScript, React JS, and jQuery. We are proficient in working with database platforms like Firebase and Sanity for efficient data management. We are well-versed in software development principles, testing, and agile methodologies. Additionally, We utilize various web development tools to streamline the development process. We thrive in high-pressure environments and excel in adaptability, problem-solving, and attention to detail. Overall, We are dedicated professional ready to contribute our expertise and drive success in any team or project.</p>
               </div>
             </div>
           </div>
